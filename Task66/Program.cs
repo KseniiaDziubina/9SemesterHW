61c18dfdf7cb856142ab3ab4432c901f72bdbc41
Kseniia <ksenjadzjubina@gmail.com>
2022-08-11 00:52:41 +0300

    Задача 66

---
+++ Task66/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число M: ");
+int NumberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int NumberN = Convert.ToInt32(Console.ReadLine());
+
+int count = NumberM;
+
+PrintSumOfNumbers(NumberM, NumberN, count = 0);
+
+void PrintSumOfNumbers(int NumberM, int NumberN, int Sum)
+{
+  Sum = Sum + NumberN;
+  if (NumberN <= NumberM)
+  {
+    Console.Write($"Сумма чисел равна: {Sum} ");
+    return;
+  }
+  PrintSumOfNumbers(NumberM, NumberN - 1, Sum);
+}

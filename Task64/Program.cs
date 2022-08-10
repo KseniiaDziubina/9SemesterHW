7596425a4d6d61751a5a82087f4aebb1bbfa31de
Kseniia <ksenjadzjubina@gmail.com>
2022-08-11 00:45:33 +0300

    Задача 64

---
+++ Task64/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число M: ");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+void printNumbers(int SecondNumber, int count = 1)
+{
+    if (count == SecondNumber)
+    {
+        Console.Write(SecondNumber);
+    }
+    else
+    {
+        Console.Write($"{count}, ");
+        printNumbers(SecondNumber, count + 1);
+    }
+}
+Console.Write($"В промежутке от {FirstNumber} до {SecondNumber}   ---> ");
+printNumbers(SecondNumber, FirstNumber);
